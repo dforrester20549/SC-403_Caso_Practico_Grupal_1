8c8fc7350f536428bb79ceadd806cbfee3721a35
dforrester20549 <123339615+dforrester20549@users.noreply.github.com>
2023-10-17 22:41:00 -0600

    Actualizacion codigo
    Se actualiza el codigo hasta donde esta actualmente se continua en el branch 3

--- InitializrSpringbootProject/src/main/java/com/practica_grupal_1/controller/CategoriaController.java
+++ InitializrSpringbootProject/src/main/java/com/practica_grupal_1/controller/CategoriaController.java
@@ -3,6 +3,8 @@ package com.practica_grupal_1.controller;
 import com.practica_grupal_1.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -12,4 +14,13 @@ public class CategoriaController {
     
     @Autowired
     private CategoriaService categoriaService;
+    
+    @GetMapping("/listado")//Ejecuta lo de la plantilla de listado y crea un url
+    public String listado (Model model){
+        
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalCategorias", categorias.size());
+        return "/categoria/listado";//es un folder en templates que vamos a crear
+    }
 }
